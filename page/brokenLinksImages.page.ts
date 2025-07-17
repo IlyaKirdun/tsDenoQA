b41bc8112fbf244b84d0d5b1a5dbf5678625417c
IlyaKirdun <agentneww@gmail.com>
2025-07-17 20:52:46 +0300

    broken-links-images
    Добавил файлы brokenLinksImages.page ; brokenLinksImages.spec(4 cases);
    
    4 теста для страницы Broken Links - Images:
    
    Проверяем правильное отображение изображения "Valid image".
    Проверяем правильное отображение изображения "Broken image".
    Проверяем ссылку "Valid link".
    Проверяем ссылку "Broken link".

---
+++ page/brokenLinksImages.page.ts
@@ -0,0 +1,57 @@
+import {BrowserContext, expect, Locator, Page} from "@playwright/test"
+
+export default class BrokenLinksImages {
+  page: Page
+  context: BrowserContext
+  validImage: Locator
+  brokenImage: Locator
+
+  imagesDirection = {
+    valid: 'https://demoqa.com/images/Toolsqa.jpg',
+    broken: 'https://demoqa.com/images/Toolsqa_1.jpg'
+  }
+
+  urlList = {
+    valid: 'https://demoqa.com/',
+    broken: 'http://the-internet.herokuapp.com/status_codes/500',
+  }
+
+  constructor(page: Page, context: BrowserContext) {
+    this.page = page
+    this.context = context
+    this.validImage = this.page.locator('//p[text()="Valid image"]//parent::div//img[@src="/images/Toolsqa.jpg"]')
+    this.brokenImage = this.page.locator('//p[text()="Valid image"]//parent::div//img[@src="/images/Toolsqa_1.jpg"]')
+  }
+
+  async verifyImageByState(imageLocator: Locator, state: 'valid' | 'broken'): Promise<void> {
+    const imageValues = await imageLocator.evaluate((image: HTMLImageElement) => {
+      return {
+        naturalWidth: image.naturalWidth,
+        naturalHeight: image.naturalHeight,
+        src: image.src
+      }
+    })
+
+    expect(imageValues.src).toBe(this.imagesDirection[state])
+
+    if(state === 'valid') {
+      expect(imageValues.naturalHeight).toBeGreaterThanOrEqual(1)
+      expect(imageValues.naturalWidth).toBeGreaterThanOrEqual(1)
+    }
+
+    if(state === 'broken') {
+      expect(imageValues.naturalHeight).toBeLessThanOrEqual(0)
+      expect(imageValues.naturalWidth).toBeLessThanOrEqual(0)
+    }
+  }
+
+  async clickAndVerifyLinkByName(linkName: 'Valid' | 'Broken', state: 'valid' | 'broken', statusCode: number): Promise<void> {
+    const response = this.page.waitForResponse(response =>
+      response.status() == statusCode
+    )
+
+    await this.page.locator(`//a[text()="Click Here for ${linkName} Link"]`).click()
+    await expect(this.page).toHaveURL(this.urlList[state])
+    await response
+  }
+}
---
+++ tests/brokenLinksImages.spec.ts
@@ -0,0 +1,49 @@
+import {test} from "@playwright/test"
+import MainPage from "../page/main.page"
+import NavigationBar from "../utils/components/navigationBar"
+import BrokenLinksImages from "../page/brokenLinksImages.page"
+import {removeAds} from "../utils/functions"
+
+test.describe('Проверка функциональности на странице "Links', () => {
+  let mainPage: MainPage
+  let navigationBar: NavigationBar
+  let brokenLinksImages: BrokenLinksImages
+
+  test.beforeEach(async ({ page , context}) => {
+    mainPage = new MainPage(page)
+    navigationBar = new NavigationBar(page)
+    brokenLinksImages = new BrokenLinksImages(page, context)
+
+    await mainPage.navigateToMainPage()
+    await removeAds(page)
+    await mainPage.clickOnElement('Elements')
+    await navigationBar.clickElementInNavigationBar('Broken Links - Images')
+    await removeAds(page)
+  })
+
+  test('CASE_1: Проверяем правильное отображение изображения "Valid image".', async () => {
+    await test.step('Проверяем отображение изображение "Valid image".', async () => {
+      await brokenLinksImages.verifyImageByState(brokenLinksImages.validImage, 'valid')
+    })
+  })
+
+  test('CASE_2: Проверяем сломанное изображения "Broken image".', async () => {
+    await test.step('Проверяем отображение изображение "Broken image".', async () => {
+      await brokenLinksImages.verifyImageByState(brokenLinksImages.brokenImage, 'broken')
+    })
+  })
+
+  test('CASE_3: Проверяем ссылку "Valid link".', async () => {
+    const statusCode = 200
+    await test.step('Нажимаем на ссылку "Valid link".', async () => {
+      await brokenLinksImages.clickAndVerifyLinkByName('Valid', 'valid', statusCode)
+    })
+  })
+
+  test('CASE_4: Проверяем ссылку "Broken link".', async () => {
+    const statusCode = 500
+    await test.step('Нажимаем на ссылку "Broken link".', async () => {
+      await brokenLinksImages.clickAndVerifyLinkByName('Broken', 'broken', statusCode)
+    })
+  })
+})

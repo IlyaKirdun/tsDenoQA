df8952e626a40abb46131dff55f104f8d823deeb
Ilya Kirdun <agentneww@gmail.com>
2025-07-22 16:22:40 +0300

    Создал файл uploadAndDownload.page Создал папку testFiles и добавил fileForUpload

---
+++ page/uploadAndDownload.page.ts
@@ -0,0 +1,41 @@
+import {BrowserContext, expect, Locator, Page} from "@playwright/test"
+import path from "path";
+import * as fs from "node:fs";
+
+export default class UploadAndDownload {
+  page: Page
+  context: BrowserContext
+  uploadFileButton: Locator
+  downloadButton: Locator
+  uploadFilePath: Locator
+
+  downloadFilePath: string = 'utils/downloads/sampleFile.jpeg'
+
+  constructor(page: Page, context: BrowserContext) {
+    this.page = page
+    this.context = context
+    this.downloadButton = this.page.locator('//a[@id="downloadButton"]')
+    this.uploadFileButton = this.page.locator('//input[@id="uploadFile"]')
+    this.uploadFilePath = this.page.locator('//p[@id="uploadedFilePath"]')
+  }
+
+  async clickDownloadButton() {
+    const downloadPromise = this.page.waitForEvent('download')
+    await this.downloadButton.click()
+    const download = await downloadPromise
+
+    await download.saveAs('utils/downloads/' + download.suggestedFilename())
+  }
+
+  async verifyDownloadFile() {
+    expect(fs.existsSync(this.downloadFilePath)).toBe(true)
+  }
+
+  async clickUploadFileButtonAndSelectFile(): Promise<void> {
+    await this.uploadFileButton.setInputFiles(path.join('utils/testFiles/fileForUpload.jpeg'))
+  }
+
+  async verifyUploadFilePath(path: string): Promise<void> {
+    await expect(this.uploadFilePath).toHaveText(path)
+  }
+}

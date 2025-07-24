5563de828363736b96addb0a89fd3f808750185e
Ilya Kirdun <agentneww@gmail.com>
2025-07-24 13:49:22 +0300

    Добавил dynamicProperties.page;dynamicProperties.spec Добавил тип ActionState в types

---
+++ tests/dynamicProperties.spec.ts
@@ -0,0 +1,81 @@
+import {expect, test} from "@playwright/test"
+import MainPage from "../page/main.page"
+import NavigationBar from "../utils/components/navigationBar"
+import DynamicProperties from "../page/dynamicProperties.page"
+import {removeAds} from "../utils/functions";
+
+test.describe('Проверка функциональности на странице "Dynamic Properties"', () => {
+  let mainPage: MainPage
+  let navigationBar: NavigationBar
+  let dynamicProperties: DynamicProperties
+
+  test.beforeEach(async ({page}) => {
+    mainPage = new MainPage(page)
+    navigationBar = new NavigationBar(page)
+    dynamicProperties = new DynamicProperties(page)
+
+    await mainPage.navigateToMainPage()
+    await removeAds(page)
+    await mainPage.clickOnElement('Elements')
+    await navigationBar.clickElementInNavigationBar('Dynamic Properties')
+    await removeAds(page)
+  })
+
+  test('CASE_1: Проверяем текст с изменяемым ID', async ({page}) => {
+    let initialTextId: string | null = ''
+    let actualTextId: string | null = ''
+
+    await test.step(`Сохраняем ID текста в ${initialTextId}.`, async () => {
+      initialTextId = await dynamicProperties.getTextId()
+    })
+
+    await test.step('Обновляем страницу.', async () => {
+      await page.reload()
+    })
+
+    await test.step(`Получаем ID и сравниваем с ${initialTextId}.`, async () => {
+      actualTextId = await dynamicProperties.getTextId()
+
+      expect(actualTextId).not.toBe(initialTextId)
+    })
+  })
+
+  test('CASE_2: Проверяем изменение состояния кнопки "Will enable 5 seconds".', async () => {
+
+    await test.step('Pre-conditions', async () => {
+      await test.step('Проверяем что кнопка не активна.', async () => {
+        await dynamicProperties.verifyButtonState('toBeDisabled')
+      })
+    })
+
+    await test.step('Ожидаем изменение состояния кнопки через 5 секунд.', async () => {
+      await dynamicProperties.verifyButtonState('toBeDisabled', 6000)
+    })
+  })
+
+  test('CASE_3: Проверяем изменение цвета текста у кнопки "Color Change".', async () => {
+
+    await test.step('Pre-conditions', async () => {
+      await test.step('Проверяем что цвет текста у кнопки "Белый".', async () => {
+        await dynamicProperties.verifyButtonColor('white')
+      })
+    })
+
+    await test.step('Ожидаем изменение цвета текста на "Красный" у кнопки через 5 секунд.', async () => {
+      await dynamicProperties.verifyButtonColor('red', 6000)
+    })
+  })
+
+  test('CASE_4: Проверяем состояния кнопки "Visible After 5 Seconds".', async () => {
+
+    await test.step('Pre-conditions', async () => {
+      await test.step('Проверяем что кнопка не отображается.', async () => {
+        await dynamicProperties.verifyButtonVisibility('toBeHidden')
+      })
+    })
+
+    await test.step('Ожидаем отображение кнопки через 5 секунд.', async () => {
+      await dynamicProperties.verifyButtonVisibility('toBeVisible', 6000)
+    })
+  })
+})

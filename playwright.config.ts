26bcab739888f6fc618884e1a4129be4cd5e7ad7
Ilya Kirdun <agentneww@gmail.com>
2025-08-08 11:41:25 +0300

    Почистил от мусора playwright.config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,27 +1,15 @@
 import { defineConfig, devices } from '@playwright/test';
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
 import dotenv from 'dotenv';
 import path from 'path';
+
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 1,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 4 : 4,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     [
       'playwright-qase-reporter',
@@ -35,17 +23,12 @@ export default defineConfig({
       },
     ],
   ],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
   use: {
     headless: false,
-    /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://demoqa.com/',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    //trace: 'on-first-retry',
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
